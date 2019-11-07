726876f22be27192a52c640ff49e9337482b4cdb
Volt <sonianadashkevich@gmail.com>
2019-11-07 22:10:00 +0100

    zadanie1 i zadanie3 poprawione

--- src/z1/Person.java
+++ src/z1/Person.java
@@ -6,16 +6,6 @@ public class Person {
     private String pesel;
     private int age;
 
-    @Override
-    public String toString() {
-        return "Person{" +
-                "firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", pesel='" + pesel + '\'' +
-                ", age=" + age +
-                '}';
-    }
-
     public Person(String firstName, String lastName, String pesel, int age)
             throws NameUndefinedException, IncorrectAgeException {
         checkPreconditions(firstName, lastName, age);
@@ -25,30 +15,26 @@ public class Person {
         this.age = age;
     }
 
-    private void checkPreconditions(String firstName, String lastName, int age)
-            throws NameUndefinedException, IncorrectAgeException {
-        if (firstName==null || lastName==null||firstName.length()<2||lastName.length()<2){
-            throw new NameUndefinedException();
-        }
-        if (age<1){
-            throw new IncorrectAgeException();
-        }
-    }
-
     public String getFirstName() {
         return firstName;
     }
 
-    public void setFirstName(String firstName) {
+    public void setFirstName(String firstName) throws NameUndefinedException{
         this.firstName = firstName;
+        if (firstName==null ||firstName.length()<2){
+            throw new NameUndefinedException();
+        }
     }
 
     public String getLastName() {
         return lastName;
     }
 
-    public void setLastName(String lastName) {
+    public void setLastName(String lastName) throws NameUndefinedException {
         this.lastName = lastName;
+        if (lastName==null||lastName.length()<2){
+            throw new NameUndefinedException();
+        }
     }
 
     public String getPesel() {
@@ -63,7 +49,31 @@ public class Person {
         return age;
     }
 
-    public void setAge(int age) {
+    public void setAge(int age) throws IncorrectAgeException {
         this.age = age;
+        if (age<1){
+            throw new IncorrectAgeException();
+        }
     }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", pesel='" + pesel + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    private void checkPreconditions(String firstName, String lastName, int age)
+            throws NameUndefinedException, IncorrectAgeException {
+        if (firstName==null || lastName==null||firstName.length()<2||lastName.length()<2){
+            throw new NameUndefinedException();
+        }
+        if (age<1){
+            throw new IncorrectAgeException();
+        }
+    }
+
 }
